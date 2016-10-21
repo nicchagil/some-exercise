a13e7207bb235e0072e559b336bf8a75edc79219
nicchagil <nic130330223@gmail.com>
2016-10-21 23:10:36 +0800

    添加注释。

--- 043Testcase4SpringTransaction/src/com/nicchagil/mybatis3spring3intg/HowToTest.java
+++ 043Testcase4SpringTransaction/src/com/nicchagil/mybatis3spring3intg/HowToTest.java
@@ -12,13 +12,13 @@ public class HowToTest {
                 new ClassPathXmlApplicationContext("/config/spring_mybatis.xml");
 		UserService userService = context.getBean("userService", UserService.class);
 		
-		// 测试一
+		// 简单地测试事务是否正常
 		userService.test1();
 		
-		// 测试二
+		// 测试进入Service的第一个方法并无包含@Transactional，而该方法调用的含有@Transactional的方法，被调用的方法的事务是否有效
 		// userService.test2Wrap();
 		
-		// 测试三
+		// 测试在调用的方法启动新事务（新方法在同一个Service与不同Service的区别）
 		// userService.test3Wrap();
 	}
 
