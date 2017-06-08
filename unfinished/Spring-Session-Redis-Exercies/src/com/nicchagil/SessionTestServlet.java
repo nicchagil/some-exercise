96a0a12c1b38ad525b3e319418ad71e3c4b26710
nicchagil <nic130330223@gmail.com>
2017-06-09 00:02:31 +0800

    commit Spring-Session-Redis-Exercies.

---
+++ unfinished/Spring-Session-Redis-Exercies/src/com/nicchagil/SessionTestServlet.java
@@ -0,0 +1,44 @@
+package com.nicchagil;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionTestServlet
+ */
+public class SessionTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Logger logger = Logger.getLogger("SessionTestServlet");
+
+    /**
+     * Default constructor. 
+     */
+    public SessionTestServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		this.logger.info("session : " + session);
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
