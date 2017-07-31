a21597d4855d3ccc7d221dac7bf3e0a83b3a9af2
nicchagil <nic130330223@gmail.com>
2017-07-31 18:16:00 +0800

    update MQTT-Spring-Integration-MQTT-Exervice.

---
+++ study-and-demo/MQTT-Spring-Integration-MQTT-Exervice/src/Consumer.bat
@@ -0,0 +1,3 @@
+title "Consumer"
+set CLASSPATH=D:\exercise_workspace\MQTT-Spring-Integration-MQTT-Exervice\target\dependency\*;D:\exercise_workspace\MQTT-Spring-Integration-MQTT-Exervice\target\classes;%CLASSPATH%
+java com.nicchagil.springintegrationmqttexervice.Boot
