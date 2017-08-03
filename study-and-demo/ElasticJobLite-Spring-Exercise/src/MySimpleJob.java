db074821a734910ad172a0d1d5d437a1c5e803cd
nicchagil <nic130330223@gmail.com>
2017-08-03 20:45:35 +0800

    update ElasticJobLite-Spring-Exercise.

--- study-and-demo/ElasticJobLite-Spring-Exercise/src/MySimpleJob.java
+++ study-and-demo/ElasticJobLite-Spring-Exercise/src/MySimpleJob.java
@@ -1,15 +1,18 @@
-import org.apache.log4j.Logger;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 
 public class MySimpleJob implements SimpleJob {
 	
-	private final Logger logger = Logger.getLogger(this.getClass());
+	private final Logger logger = LoggerFactory.getLogger(MySimpleJob.class);
 
 	@Override
 	public void execute(ShardingContext shardingContext) {
-		logger.info(shardingContext.getShardingItem());
+		logger.info("ABC - " + shardingContext);
 	}
 	
 }
