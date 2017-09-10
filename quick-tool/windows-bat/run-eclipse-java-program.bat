90123add24dd4ef78ca5c37333a81035ad0f51cd
nicchagil <nic130330223@gmail.com>
2017-09-10 17:11:35 +0800

    update run-eclipse-java-program.bat.

--- quick-tool/windows-bat/run-eclipse-java-program.bat
+++ quick-tool/windows-bat/run-eclipse-java-program.bat
@@ -3,8 +3,7 @@
 :: 设置执行文件名为标题
 title %0
 
-:: 在pom.xml所在目录下执行mvn -f pom.xml dependency:copy-dependencies，下载依赖JAR包
-:: 拷贝此脚本到class目录下，执行所调用的Java类
+:: 在pom.xml所在目录下执行mvn -f pom.xml dependency:copy-dependencies，下载依赖JAR包。拷贝此脚本到class目录下，执行所调用的Java类
 java -cp "..\dependency\*;.\classes;%CLASSPATH%" com.xxxxx.test.Boot
 
 :: 执行完等待输入
