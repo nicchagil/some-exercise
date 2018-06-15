b3f9bf329b32b99ea532b55e7572bc484725a8de
nicchagil <nic130330223@gmail.com>
2018-06-16 00:35:11 +0800

    添加第三方框架的使用测试用例。

---
+++ study-and-demo/wc-user/src/test/java/com/nicchagil/exercise/wcuser/thirdpartyframework/map2bean/Map2BeanByBeanUtils.java
@@ -0,0 +1,94 @@
+package com.nicchagil.exercise.wcuser.thirdpartyframework.map2bean;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.junit.Test;
+
+public class Map2BeanByBeanUtils {
+
+	@Test
+	public void test1() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", 123);
+		map.put("name", "Nick");
+
+		User user = new User();
+		try {
+			BeanUtils.populate(user, map);
+			System.out.println("user : " + user);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void test2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", "123");
+		map.put("name", "Nick");
+
+		User user = new User();
+		try {
+			BeanUtils.populate(user, map);
+			System.out.println("user : " + user);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void test3() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", "123");
+		map.put("name", "Nick");
+		map.put("birthday", new Date());
+		
+		User user = new User();
+		try {
+			BeanUtils.populate(user, map);
+			System.out.println("user : " + user);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static class User {
+		private Integer id;
+		private String name;
+		private Date birthday;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Date getBirthday() {
+			return birthday;
+		}
+
+		public void setBirthday(Date birthday) {
+			this.birthday = birthday;
+		}
+
+		@Override
+		public String toString() {
+			return "User [id=" + id + ", name=" + name + ", birthday=" + birthday + "]";
+		}
+	}
+
+}
