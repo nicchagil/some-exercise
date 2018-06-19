16071b734d1e1699f9a87e26d70f2e5879c49010
nicchagil <nic130330223@gmail.com>
2018-06-19 23:48:30 +0800

    使用自定义的Converter将时间字符串转换为Date。

--- study-and-demo/wc-user/src/test/java/com/nicchagil/exercise/wcuser/thirdpartyframework/map2bean/Map2BeanByBeanUtils.java
+++ study-and-demo/wc-user/src/test/java/com/nicchagil/exercise/wcuser/thirdpartyframework/map2bean/Map2BeanByBeanUtils.java
@@ -1,6 +1,8 @@
 package com.nicchagil.exercise.wcuser.thirdpartyframework.map2bean;
 
 import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +10,7 @@ import java.util.Map;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtilsBean;
+import org.apache.commons.beanutils.Converter;
 import org.apache.commons.beanutils.locale.converters.IntegerLocaleConverter;
 import org.junit.Test;
 
@@ -85,6 +88,46 @@ public class Map2BeanByBeanUtils {
 		try {
 			ConvertUtilsBean convertUtilsBean = new ConvertUtilsBean();
 			convertUtilsBean.register(new IntegerLocaleConverter(), Integer.class);
+			
+			BeanUtilsBean beanUtilsBean = new BeanUtilsBean(convertUtilsBean);
+			beanUtilsBean.populate(user, map);
+			
+			System.out.println("user : " + user);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void test5x1() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", "123");
+		map.put("name", "Nick");
+		map.put("birthday", "2018-06-18 08:56:47");
+		
+		User user = new User();
+		try {
+			ConvertUtilsBean convertUtilsBean = new ConvertUtilsBean();
+			convertUtilsBean.register(new Converter() {
+
+				@Override
+				public <T> T convert(Class<T> type, Object value) {
+					if (value == null) {
+						return null;
+					}
+					
+					if (Date.class.getName().equals(type.getName())) {
+						try {
+							return (T) new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(value.toString());
+						} catch (ParseException e) {
+							throw new RuntimeException(e);
+						}
+					}
+					
+					return null;
+				}
+				
+			}, Date.class);
 			
 			BeanUtilsBean beanUtilsBean = new BeanUtilsBean(convertUtilsBean);
 			beanUtilsBean.populate(user, map);
