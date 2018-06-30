742f56ae4b46ae3b3b77eed494e354d1fcc028f4
nicchagil <nic130330223@gmail.com>
2018-06-30 16:03:12 +0800

    补充启动脚本。

--- study-and-demo/wc-user/run-by-java-jar.bat
+++ study-and-demo/wc-user/run-by-java-jar.bat
@@ -1,2 +1,4 @@
+set dateTimeStr=%date:~0,4%-%date:~5,2%-%date:~8,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%
+
 call mvn clean package -Dmaven.test.skip=true
-java -jar -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseG1GC -Xms1024m -Xmx1024m -Xss1024k -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./dump/ ./target/wc-user-0.0.1-SNAPSHOT.jar
+java -jar -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:./gc.log -XX:+UseG1GC -Xms1024m -Xmx1024m -Xss1024k -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./dump/ ./target/wc-user-0.0.1-SNAPSHOT.jar
